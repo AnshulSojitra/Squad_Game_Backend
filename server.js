4ca22d76ce427f7b9afad20463b514b226cd425d
Anshul <anshul.patel321@gmail.com>
2025-12-24 12:03:45 +0530

    db.js

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 require("dotenv").config();
 const app = require("./app");
-const mongoose = require("mongoose");
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error(err));
+const connectDB = require("./config/db");
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
