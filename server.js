4ef615f6459955bcab4ab63abea18af3b035e8c7
Anshul <anshul.patel321@gmail.com>
2026-02-23 10:43:21 +0530

    safe

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 require("dotenv").config();
 const app = require("./app");
+const sequelize = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+// sequelize.authenticate()
+//   .then(() => {
+//     console.log("✅ Database connected successfully");
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`🚀 Server running on port ${process.env.PORT}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.error("❌ Database connection failed:", err);
+//   });
